aa4b433a481f011020917c366f4d8b1b3b89d0c3
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2022-12-01 12:08:08 +0300

    fix: checker errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,12 @@ import sys
 if __name__ == "__main__":
     n = len(sys.argv)
     if n == 1:
-        print("0 arguments")
+        print("{:d} {}.".format(0, "arguments"))
     elif n == 2:
-        print("1 argument")
+        print("{:d} {}:".format(1, "argument"))
+        for i in range(1, n):
+            print("{:d}: {}".format(n - 1, sys.argv[i]))
     else:
         print("{:d} {}:".format(n - 1, "arguments"))
-    for i in range(1, n):
-        print("{:d}: {}".format(i, sys.argv[i]))
+        for i in range(1, n):
+            print("{:d}: {}".format(i, sys.argv[i]))
