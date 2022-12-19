1bd6ad105d75b44981498a3341e732482501b23a
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2022-12-19 10:05:35 +0300

    fix: pycodestyle

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,6 @@ def safe_print_division(a, b):
         div = a / b
     except (TypeError, ZeroDivisionError):
         div = None
-    finally: 
+    finally:
         print("Inside result: {}".format(div))
     return div
