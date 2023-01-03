614cc43ffb9e9f3f89440acb589150e29fdcd011
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2023-01-03 16:04:56 +0300

    fix: task 3 (attempt)

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,9 +47,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
 
+        rect = []
         for i in range(self.__height):
-            for j in range(self.__width):
-                print("#", end="")
+            [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
-                print()
-        return ""
+                rect.append("\n")
+        return "".join(rect)
