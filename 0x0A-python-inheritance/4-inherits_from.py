528ff638339abb9763ee9ee30db27989006d30b8
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2023-01-09 17:23:11 +0300

    fix: errors + add: task 13

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""check if object if instance of a class that
-inherited from specified class"""
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    """return True if object is an instance of a class that
-    inherited from specified class"""
-    if issubclass(obj.__class__, a_class) is True:
-        if obj.__class__ is not a_class:
-            return True
-    else:
-        return False
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
